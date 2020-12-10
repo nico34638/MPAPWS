95c87542906ee4784bcb8d8f6194c00e140d78db
Remi Mignon <remi.mignon@etudiant.univ-lr.fr>
2020-12-10 15:41:14 +0100

    apelle a la bd pour l'accueil + fix dans nav bar

--- mpapws/src/Controller/IndexController.php
+++ mpapws/src/Controller/IndexController.php
@@ -2,6 +2,11 @@
 
 namespace App\Controller;
 
+use App\Entity\Produit;
+use App\Domain\Query\ListeProducteursHandler;
+use App\Domain\Query\ListeProducteursQuery;
+use App\Domain\Query\ListeProduitsHandler;
+use App\Domain\Query\ListeProduitsQuery;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
@@ -15,9 +20,30 @@ class IndexController extends AbstractController
 {
     /**
      * @Route("/", name="home")
+     * @param ListeProduitsHandler $handler
+     * @return Response
      */
-    public function index(): Response
+    public function index(ListeProduitsHandler $productHandler,ListeProducteursHandler $producerHandler): Response
     {
-        return $this->render('index/index.html.twig');
+        $productQuery= new ListeProduitsQuery();
+        $allProducts= $productHandler->handle($productQuery);
+
+        $producerQuery= new ListeProducteursQuery();
+        $allProducers= $producerHandler->handle($producerQuery);
+
+
+        $nbProducts = count($allProducts);
+        $nbProducer = count($allProducers);
+        $products = array();
+
+        for ($i = 1; $i <= 12; $i++){
+            array_push($products,$allProducts[rand(0,$nbProducts-1)]);
+        }
+
+        return $this->render('index/index.html.twig', [
+            'products'=>$products,
+            'nbProducts'=>$nbProducts,
+            'nbProducer'=>$nbProducer,
+        ]);
     }
 }
