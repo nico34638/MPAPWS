18a3b1a979ad4dbe8cc93b587c105a75cd3d47df
lguill12 <louis.guillet1@etudiant.univ-lr.fr>
2020-12-10 11:10:23 +0100

    randomize name of vegetebale and fruits

--- mpapws/src/DataFixtures/ProductFixtures.php
+++ mpapws/src/DataFixtures/ProductFixtures.php
@@ -14,22 +14,24 @@ class ProductFixtures extends Fixture
         // $product = new Product();
         // $manager->persist($product);
 
-        $faker = Faker\Factory::create('fr_FR');
+        $faker = \Faker\Factory::create('fr_FR');
+        $faker->addProvider(new \FakerRestaurant\Provider\fr_FR\Restaurant($faker));
 
         for ($i = 0; $i < 20; $i++)
         {
             $produit = new Produit();
             if ($i % 2 == 0)
             {
-                $produit->setNom("Bouglour");
+                $produit->setNom($faker->vegetableName());
+                $produit->setSourceImage("https://via.placeholder.com/150/93A8AC/000000?Text=FarMeetic");
             } else
             {
-                $produit->setNom("Carotte");
+                $produit->setNom($faker->fruitName());
+                $produit->setSourceImage("https://via.placeholder.com/150/424B54/FFFFFF?Text=FarMeetic");
             }
 
             $produit->setDescription($faker->text);
             $produit->setPrix(random_int(1, 155));
-            $produit->setSourceImage("https://via.placeholder.com/150");
             $manager->persist($produit);
         }
         $manager->flush();
