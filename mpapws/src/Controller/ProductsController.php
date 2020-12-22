0b9b55a6fe7cfeddc752d8b6d0a5f17c8bd286ff
Nicolas Fidel <nicolas.fidel@etudiant.univ-lr.fr>
2020-12-22 15:23:19 +0100

    add upload file

--- mpapws/src/Controller/ProductsController.php
+++ mpapws/src/Controller/ProductsController.php
@@ -9,10 +9,12 @@ use App\Domain\Query\ListProductsQuery;
 use App\Entity\Product;
 use App\Form\ProductType;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\File\Exception\FileException;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Component\Security\Core\Security;
+use Symfony\Component\String\Slugger\SluggerInterface;
 
 class ProductsController extends AbstractController
 {
@@ -23,29 +25,60 @@ class ProductsController extends AbstractController
      */
     public function listPrd(ListProductsHandler $handler): Response
     {
-        $query= new ListProductsQuery();
-        $products= $handler->handle($query);
+        $query = new ListProductsQuery();
+        $products = $handler->handle($query);
 
         return $this->render('products/listProducts.html.twig', [
-            'products'=> $products,
+            'products' => $products,
         ]);
     }
 
     /**
      * @Route("/produits/admin/ajouter", name="addProduct")
      * @param Request $request
+     * @param AddProductHandler $handler
+     * @param Security $security
+     * @param SluggerInterface $slugger
+     * @return Response
      */
-    public function addProduct(Request $request, AddProductHandler $handler, Security $security): Response
+    public function addProduct(Request $request, AddProductHandler $handler, Security $security, SluggerInterface $slugger): Response
     {
         $product = new Product();
         $form = $this->createForm(ProductType::class, $product);
         $form->handleRequest($request);
 
-        if($form->isSubmitted() and $form->isValid())
+        if ($form->isSubmitted() && $form->isValid())
         {
-            $product->setSourceImage('https://via.placeholder.com/150/93A8AC/000000?Text=FarMeetic');
-            $product->setProducers($security->getUser());
 
+            $file = $form->get('imageFile')->getData();
+            if ($file)
+            {
+                $originalFilename = pathinfo($file->getClientOriginalName(), PATHINFO_FILENAME);
+                // this is needed to safely include the file name as part of the URL
+                $safeFilename = $slugger->slug($originalFilename);
+                $newFilename = $safeFilename . '-' . uniqid() . '.' . $file->guessExtension();
+                // Move the file to the directory where brochures are stored
+                try
+                {
+                    $file->move(
+                        $this->getParameter('products_directory'),
+                        $newFilename
+                    );
+                } catch (FileException $e)
+                {
+                    // ... handle exception if something happens during file upload
+                }
+
+                // updates the 'brochureFilename' property to store the PDF file name
+                // instead of its contents
+                $product->setSourceImage("uploads/products/" . $newFilename);
+            }
+            else
+            {
+                $product->setSourceImage('https://via.placeholder.com/150/93A8AC/000000?Text=FarMeetic');
+            }
+
+            $product->setProducers($security->getUser());
             $command = new AddProductCommand($product);
             $handler->handle($command);
         }
--- mpapws/src/Form/ProductType.php
+++ mpapws/src/Form/ProductType.php
@@ -4,6 +4,7 @@ namespace App\Form;
 
 use App\Entity\Product;
 use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\FileType;
 use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
@@ -27,6 +28,11 @@ class ProductType extends AbstractType
             ->add('name', TextType::class, ['label' => 'Nom du produit'])
             ->add('description', TextType::class, ['label' => 'Description du produit'])
             ->add('price', IntegerType::class, ['label' => 'Prix du produit'])
+            ->add('imageFile', FileType::class, [
+                'label' => 'Image du produit',
+                'mapped' => false,
+                'required' => false
+            ])
         ;
     }
 
