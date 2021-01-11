b381f3df4c9aa577ce505fc7d52278fcfa7a3946
Nicolas Fidel <nicolas.fidel@etudiant.univ-lr.fr>
2021-01-11 22:06:00 +0100

    add list favorites

--- mpapws/src/Controller/ProducerController.php
+++ mpapws/src/Controller/ProducerController.php
@@ -2,8 +2,11 @@
 
 namespace App\Controller;
 
+use App\Domain\Query\ListOfFavoritesHandler;
+use App\Domain\Query\ListOfFavoritesQuery;
 use App\Domain\Query\ListProducersHandler;
 use App\Domain\Query\ListProducersQuery;
+use App\Repository\UserRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
@@ -15,13 +18,27 @@ class ProducerController extends AbstractController
      * @param ListProducersHandler $handler
      * @return Response
      */
-    public function index(ListProducersHandler $handler): Response
+    public function index(ListProducersHandler $handler, UserRepository $repository, ListOfFavoritesHandler $favoritesHandler): Response
     {
         $query = new ListProducersQuery();
         $producers = $handler->handle($query);
 
+        $id_tab = [];
+        if ($this->getUser())
+        {
+            $queryFavorites = new ListOfFavoritesQuery($this->getUser());
+            $favorites = $favoritesHandler->handle($queryFavorites);
+
+
+            foreach ($favorites as $value)
+            {
+                array_push($id_tab, $value->getId());
+            }
+        }
+
         return $this->render('producers/listProducers.html.twig', [
-            'producers' => $producers
+            'producers' => $producers,
+            'followings' => $id_tab
         ]);
     }
 }
