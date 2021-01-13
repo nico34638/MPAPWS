3797e5f1a90c9442747584effb6ec763ac81a5df
Remi Mignon <remi.mignon@etudiant.univ-lr.fr>
2021-01-13 20:25:11 +0100

    etoil sur liste dse favorie

--- mpapws/src/Controller/FavoritesController.php
+++ mpapws/src/Controller/FavoritesController.php
@@ -83,18 +83,48 @@ class FavoritesController extends AbstractController
         return $this->redirectToRoute('detailproducers',['username'=>$producer->getUsername()]);
     }
 
+
+
+    /**
+     * @Route("/favoris/deleteFromList/{producer}", name="deleteFavoritesFromList")
+     * @param User $producer
+     * @param DeleteFollowingHandler $handler
+     * @return Response
+     */
+    public function deleteFavoriteFromList(User $producer, DeleteFollowingHandler $handler): Response
+    {
+        $currentUser = $this->getUser();
+        $command  = new DeleteFollowingCommand($currentUser, $producer);
+        $handler->handle($command);
+
+        return $this->redirectToRoute('listFavorites');
+    }
+
     /**
      * @Route("/favoris", name="listFavorites")
      * @param ListOfFavoritesHandler $handler
      * @return Response
      */
-    public function listFavorites(ListOfFavoritesHandler $handler):Response
+    public function listFavorites(ListOfFavoritesHandler $handler, ListOfFavoritesHandler $favoritesHandler):Response
     {
         $query = new ListOfFavoritesQuery($this->getUser());
         $favorites = $handler->handle($query);
 
+        $id_tab = [];
+        if ($this->getUser())
+        {
+            $queryFavorites = new ListOfFavoritesQuery($this->getUser());
+            $favorites = $favoritesHandler->handle($queryFavorites);
+
+
+            foreach ($favorites as $value)
+            {
+                array_push($id_tab, $value->getId());
+            }
+        }
         return $this->render('favorites/listOfFavorites.html.twig', [
-            'favorites'=> $favorites
+            'favorites'=> $favorites,
+            'followings' => $id_tab
             ]);
     }
 
