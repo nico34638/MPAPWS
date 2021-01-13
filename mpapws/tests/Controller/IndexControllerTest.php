567c643870987ac7112ba20d0142c21cf40adb9a
Nicolas Fidel <nicolas.fidel@etudiant.univ-lr.fr>
2021-01-13 18:36:18 +0100

    add test list product in home page

--- mpapws/tests/Controller/IndexControllerTest.php
+++ mpapws/tests/Controller/IndexControllerTest.php
@@ -34,4 +34,13 @@ class IndexControllerTest extends WebTestCase
         $this->client->request('GET', '/');
         $this->assertEquals(200,  $this->client->getResponse()->getStatusCode());
     }
+
+    public function test_list_of_products_in_index_page()
+    {
+        $crawler = $this->client->request('GET', '/');
+        $this->assertEquals(200,  $this->client->getResponse()->getStatusCode());
+        $crawler->filter('div.card');
+        $this->assertCount(12, $crawler->filter('div.card'));
+    }
+
 }
